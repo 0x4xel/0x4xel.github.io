c5a9fc3b52f0e475c42f487fe61f521698ef0031
0x4xel <axellodosa@gmail.com>
2022-12-15 22:37:39 +0100

    update tittle

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <title>0x4xel - Personal Blog</title>
+        <title>0x4xel - Blog Personal</title>
 
         <link
           href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap&subset=cyrillic"
