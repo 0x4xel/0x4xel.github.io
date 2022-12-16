a288c5de2bee09a1bc0e6786f6b2e5b628246d80
0x4xel <axellodosa@gmail.com>
2022-12-16 15:13:05 +0100

    update timeline on mobile

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,8 +1,37 @@
 import { Chrono } from "react-chrono";
 import items from "../../json/roadmap.json"
 import { useTranslation } from "next-i18next";
+import { useState, useCallback, useEffect } from 'react';
+const useMediaQuery = (width) => {
+    const [targetReached, setTargetReached] = useState(false);
+  
+    const updateTarget = useCallback((e) => {
+      if (e.matches) {
+        setTargetReached(true);
+      } else {
+        setTargetReached(false);
+      }
+    }, []);
+  
+    useEffect(() => {
+      const media = window.matchMedia(`(max-width: ${width}px)`);
+      media.addListener(updateTarget);
+  
+      // Check on mount (callback is not called until a change occurs)
+      if (media.matches) {
+        setTargetReached(true);
+      }
+  
+      return () => media.removeListener(updateTarget);
+    }, []);
+  
+    return targetReached;
+  };
+
 export const Timeline = () => {
-  const { t } = useTranslation("common");
+    const { t } = useTranslation("common");
+    const isMobile=useMediaQuery(600)
+
     return (
         <div className="section custom-text" id="section-timeline">
             <div className="content">
@@ -11,12 +40,13 @@ export const Timeline = () => {
                 </div>
             <div className="content-box">
                 <div class ="ancho100">
-                    <Chrono items={items} mode="VERTICAL_ALTERNATING" useReadMore="true" scrollable  cardHeight="400"
+                    <Chrono items={items} mode={`${isMobile ? "VERTICAL" : "VERTICAL_ALTERNATING"}`} borderLessCards="true" useReadMore="true" scrollable  cardHeight="400"
                      classNames={{
                         card: 'roadmap-step',
                         cardTitle: 'titulo-tarjeta',
                         cardText: 'texto-tarjeta',
-                        cardMedia: "media-tarjeta"
+                        cardMedia: "media-tarjeta",
+                        title: "fecha-tarjeta",
                     }}
                     theme={{
                         primary: 'white',
@@ -33,6 +63,7 @@ export const Timeline = () => {
             </div>
         </div>
     )
+
 };
 
 export default Timeline;
