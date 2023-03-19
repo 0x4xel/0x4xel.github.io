8143e9629f61c4726122ae2d62b7a68f47d32220
0x4xel <axellodosa@gmail.com>
2023-03-19 14:02:38 +0100

    por que si

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'Te has perdio amig@? El kebab mas cercano esta abierto, ve por uno' })
 }
